42e64a944bcfc0ac715e8c95c39e448a8ca82dd6
stanislasbdx <perso@stan1712.com>
2019-07-09 18:13:46 +0200

    Base of the fire hose

--- src/fr/stan1712/firequip/Events.java
+++ src/fr/stan1712/firequip/Events.java
@@ -1,6 +1,13 @@
 package fr.stan1712.firequip;
 
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class Events implements Listener {
     @SuppressWarnings("unused")
@@ -9,6 +16,24 @@ public class Events implements Listener {
     public Events(Main pl) {
         this.pl = pl;
         pl.getConfig();
+    }
+    
+    @EventHandler
+    public void onPlayerUse(PlayerInteractEvent event){
+        Player player = event.getPlayer();
+        if(event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+        	
+        	// lance a eau
+        	if(player.hasPermission("firequip.hose")) {
+	        	Location loc = player.getEyeLocation();
+	            World world = player.getWorld();
+	            double maxLength = 5;
+	            for(double d = 0; d <= maxLength; d += 0.5){
+	                loc.add(loc.getDirection());
+	                world.spawnParticle(Particle.WATER_SPLASH, loc, 100);
+	            }
+        	}
+	    }
     }
 	
 }
