5ab4e2326242eb28ac91a92340bafba42e4c24cb
Stanislas Castaybert <perso@stan1712.com>
2026-08-28 22:29:34 +0200

    :bug: Fix Purpur server detection

--- src/main/java/fr/stan1712/wetston/fireequipment/Main.java
+++ src/main/java/fr/stan1712/wetston/fireequipment/Main.java
@@ -30,7 +30,7 @@ public class Main extends JavaPlugin {
 
 		final Pattern versionPattern = Pattern.compile("\\d[.]\\d+", Pattern.MULTILINE);
 		final Matcher versionMatcher = versionPattern.matcher(serverVersion);
-		if(!versionMatcher.find() || (!serverType.contains("Spigot") && !serverType.contains("Paper"))) {
+		if(!versionMatcher.find() || (!serverType.contains("Spigot") && !serverType.contains("Paper") && !serverType.contains("Purpur"))) {
 			_log.error("[{}] * Server type {} unknown, disabling plugin.", logStep, serverVersion);
 
 			pluginManager.disablePlugin(this);
